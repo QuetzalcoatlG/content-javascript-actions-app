24ef335aa751296301e9062336a7f2e9b0a7b0ca
QuetzalcoatlG <quetzalcoatl.garcia@gmail.com>
2020-10-15 16:11:45 +0000

    updated sintaxis

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ chmod +x entrypoint.sh
 #!/bin/sh -l
 echo "Hello $1" # Uses who to greet input variable to print "Hello [who to greet] in the log file"
 time=$(date)    # gets time and set it as a output variable that actions run later in the job can use
-echo ""::set-output name=time::$time" 
-                # sets the action's output paramenter using workflow  sintaxis
+echo "::set-output name=time::$time"  # sets the action's output paramenter using workflow  sintaxis
